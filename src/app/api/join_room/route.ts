f5719cab77ae2fddb7811f0dd173307fa03f6b23
David Silva <daverhan02@gmail.com>
2024-05-19 16:46:53 -0400

    Perform checks to see if a user can join a room

--- src/app/api/join_room/route.ts
+++ src/app/api/join_room/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getIronSession } from "iron-session";
 import sessionOptions from "../../../lib/session";
+import { getLobby } from "../../../lib/redis";
 
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
@@ -12,6 +13,30 @@ export async function POST(req: NextRequest) {
       { error: "Room code or username was not entered" },
       { status: 400 }
     );
+  }
+
+  const lobby = await getLobby(roomCode.toString());
+
+  if (lobby) {
+    if (lobby.gameState !== "create_selections") {
+      return NextResponse.json(
+        {
+          error: "The room has already started",
+          error_code: "ROOM_HAS_STARTED",
+        },
+        { status: 403 }
+      );
+    }
+
+    if (lobby.users.find((user) => user.username === username)) {
+      return NextResponse.json(
+        {
+          error: "The username is unavailable",
+          error_code: "USERNAME_UNAVAILABLE",
+        },
+        { status: 403 }
+      );
+    }
   }
 
   const res = NextResponse.json({ roomCode });
--- src/components/JoinRoomForm.tsx
+++ src/components/JoinRoomForm.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function JoinRoomForm() {
+  const [errorMessage, setErrorMessage] = useState<string>("");
   const router = useRouter();
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
@@ -16,31 +17,47 @@ export default function JoinRoomForm() {
     });
 
     if (response.ok) router.push("/lobby/create_selections");
+    else {
+      const responseJSON = await response.json();
+      const errorMessagePrefix = "Unable to join the room";
+
+      if (
+        responseJSON.error_code === "ROOM_HAS_STARTED" ||
+        responseJSON.error_code === "USERNAME_UNAVAILABLE"
+      )
+        setErrorMessage(`${errorMessagePrefix}: ${responseJSON.error}`);
+      else setErrorMessage(errorMessagePrefix);
+    }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-col text-xl items-center gap-6"
-    >
-      <div className="flex flex-col gap-4 text-xl">
-        <input
-          placeholder="Room Code"
-          type="text"
-          name="room_code"
-          id="room_code"
-        ></input>
-        <input
-          placeholder="Username"
-          type="text"
-          name="username"
-          id="username"
-          autoComplete="off"
-        ></input>
-      </div>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-36">
-        Join Room
-      </button>
-    </form>
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col text-xl items-center gap-6"
+      >
+        <div className="flex flex-col gap-4 text-xl">
+          <input
+            placeholder="Room Code"
+            type="text"
+            name="room_code"
+            id="room_code"
+          ></input>
+          <input
+            placeholder="Username"
+            type="text"
+            name="username"
+            id="username"
+            autoComplete="off"
+          ></input>
+        </div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-36">
+          Join Room
+        </button>
+      </form>
+      {errorMessage ? (
+        <h2 className="mt-6 text-2xl text-center">{errorMessage}</h2>
+      ) : null}
+    </div>
   );
 }
